494689107cdc4995e183cbdb6058f15beefeb595
Chaance Graves <chaanceg123@gmail.com>
2020-11-02 11:27:19 -0800

    adding key commands to get docker-compose
    adding key commands to get docker-compose

--- src/install_docker.sh
+++ src/install_docker.sh
@@ -1,7 +1,7 @@
-######################
+##########################
 Docker Installation
-Tested on Ubuntu 18.04
-######################
+Tested on Ubuntu 18.04 VM
+##########################
 
 #!/bin/sh
 sudo apt-get update
@@ -18,15 +18,18 @@ echo "Update Docker Debian repository!"
 echo "+++++++++++++++++++++++++++++++++++++++++"
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-echo "+++++++++++++++++++++++++++++++++++++++++"
-echo "Docker installation begins!"
-echo "+++++++++++++++++++++++++++++++++++++++++"
+echo "+++++++++++++++++++++++++++++++++++++++++++++"
+echo "Docker & Docker-Compose installation begins!"
+echo "+++++++++++++++++++++++++++++++++++++++++++++"
 sudo apt install -y docker-ce
 sudo apt install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 docker container run hello-world
 echo "+++++++++++++++++++++++++++++++++++++++++"
 echo "Docker Installation complete!"
 docker --version
+docker-compose --version
 echo "+++++++++++++++++++++++++++++++++++++++++"
 
 ## Run Docker command without Sudo
