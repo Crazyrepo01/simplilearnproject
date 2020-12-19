dc1cb3edb4fbcdf87910a60cb8db7acdbbed3778
ctg123 <chaanceg123@gmail.com>
2020-12-19 14:18:20 +0000

    updated the install_docker script.

--- src/install_docker.sh
+++ src/install_docker.sh
@@ -1,14 +1,14 @@
 ##########################
 #Docker Installation
-#Tested on Ubuntu 18.04 VM
+#Tested on Ubuntu 20.04 VM
 ##########################
 
 #!/bin/sh
-sudo apt-get update
+sudo apt update
 echo "+++++++++++++++++++++++++++++++++++++++++"
 echo "Installing utilities!"
 echo "+++++++++++++++++++++++++++++++++++++++++"
-sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 echo "+++++++++++++++++++++++++++++++++++++++++"
 echo "Adding GPG Key!"
 echo "+++++++++++++++++++++++++++++++++++++++++"
@@ -17,12 +17,12 @@ echo "+++++++++++++++++++++++++++++++++++++++++"
 echo "Update Docker Debian repository!"
 echo "+++++++++++++++++++++++++++++++++++++++++"
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
+sudo apt update
 echo "+++++++++++++++++++++++++++++++++++++++++++++"
 echo "Docker & Docker-Compose installation begins!"
 echo "+++++++++++++++++++++++++++++++++++++++++++++"
-sudo apt install -y docker-ce
-sudo apt install docker-compose
+sudo apt-get install docker-ce
+sudo apt-get install docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker container run hello-world
